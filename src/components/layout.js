1a7dc150dda3a468f32fd15bdeb562b7e1ad9e7a
Fede Parodi <federico.parodi@sovanta.com>
2020-07-01 11:29:30 -0300

    updated url

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import './layout.css';
 import { IdentityContextProvider } from 'react-netlify-identity-widget';
 
 const Layout = ({ children }) => (
-    <IdentityContextProvider url="https://jamstack-intro-auth.netlify.com"> 
+    <IdentityContextProvider url="https://fedes-jamstack-intro-auth.netlify.app"> 
     {/* I should've created a new netlify site with all of the auth things enabled but hell I don't want to create another repo and set it up */}
     <header>
         <Link to="/">JAMstack App</Link>
